296b2bda7bbf68076e0535676ac7ed0a4ccdaf10
Cris <mirocristy@gmail.com>
2022-05-16 00:40:58 +0200

    primer commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let nombre = 'Cris';
+let edad = 28;
+let desarrolador = true;
+let fechaAniversario = new Date(1993,8,18);
+
+
+const libroFavorito = {
+    titulo: 'Nada',
+    autor: 'Carmen Laforet',
+    fecha: new Date(1945,0,6),
+    url: 'https://www.casadellibro.com/libro-nada/9788423342792/1776203',
+
+}
+
